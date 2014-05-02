c416ffd08f60bec66d97c868fa5745e62b63859a
Emil Sjölander <sjolander.emil@gmail.com>
2014-05-02 10:20:01 +0200

    added test for routing

---
+++ rest_test.go
@@ -0,0 +1,59 @@
+package rest
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestRoute(t *testing.T) {
+	matched := ""
+	routes := &Routes{
+		"/": &Methods{
+			Get: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				matched = "/"
+			}),
+		},
+		"/hi": &Routes{
+			"/": &Methods{
+				Get: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					matched = "/hi"
+				}),
+			},
+			"/there": &Methods{
+				Get: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					matched = "/hi/there"
+				}),
+			},
+		},
+	}
+
+	ErrorHandler = func(w http.ResponseWriter, err *HttpError) {
+		matched = "error"
+	}
+
+	var r *http.Request
+
+	r, _ = http.NewRequest("GET", "htttp://www.test.com/", nil)
+	routes.ServeHTTP(nil, r)
+	if matched != "/" {
+		t.Error("root route did not match, matched = " + matched)
+	}
+
+	r, _ = http.NewRequest("GET", "htttp://www.test.com/hi/", nil)
+	routes.ServeHTTP(nil, r)
+	if matched != "/hi" {
+		t.Error("/hi route did not match, matched = " + matched)
+	}
+
+	r, _ = http.NewRequest("GET", "htttp://www.test.com/hi/there", nil)
+	routes.ServeHTTP(nil, r)
+	if matched != "/hi/there" {
+		t.Error("/hi/there route did not match, matched = " + matched)
+	}
+
+	r, _ = http.NewRequest("GET", "htttp://www.test.com/hi/there/error", nil)
+	routes.ServeHTTP(nil, r)
+	if matched != "error" {
+		t.Error("error handler did not match, matched = " + matched)
+	}
+}
